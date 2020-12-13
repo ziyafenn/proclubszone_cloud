281734ac774ad5bd5468a0401f47fe3e8ada66c8
ziyafenn <ziya.fenn@gmail.com>
2020-12-13 16:25:38 +0100

    organize function

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,13 +11,11 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
+    project: "./functions/tsconfig.json",
+    // tsconfigRootDir: "/users/zia/dev/proclubszone/cloud/functions",
     sourceType: "module",
   },
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "@typescript-eslint/adjacent-overload-signatures": "error",
     "@typescript-eslint/no-empty-function": "error",
---
+++ functions/src/scheduleMatches.ts
@@ -0,0 +1,85 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+export const scheduleMatches = functions.https.onCall((data, context) => {
+  const firestore = admin.firestore();
+
+  const leagueId = "52Gt1trcnvRMCMfJdhgZ";
+  const matchesAgainst = 1;
+  const acceptedClubs: string[] = [];
+
+  interface Match {
+    home: string;
+    away: string;
+    date?: string;
+    id: number;
+    teams: [string, string];
+  }
+
+  const getTeams = async () => {
+    const query = firestore
+      .collection("leagues")
+      .doc(leagueId)
+      .collection("clubs")
+      .where("accepted", "==", true);
+    await query.get().then((querySnapshot) => {
+      querySnapshot.forEach((documentSnapshot) => {
+        const clubId = documentSnapshot.id;
+        acceptedClubs.push(clubId);
+        console.log(acceptedClubs.length, acceptedClubs);
+      });
+    });
+  };
+
+  const createMatches = () => {
+    const batch = firestore.batch();
+
+    const teams: number = acceptedClubs.length;
+    const ids: number[] = Array.from({ length: teams * 3 }, (_, i) => i + 1);
+
+    const matchIdGen = () => {
+      const randomIndex = Math.floor(
+        Math.random() * Math.floor(ids.length - 1)
+      );
+      const pickedId: number[] = ids.splice(randomIndex, 1);
+
+      return pickedId[0];
+    };
+
+    for (let r = 0; r < matchesAgainst; r++) {
+      for (let ht = 0; ht < teams; ht++) {
+        for (let at = 0; at < teams; at++) {
+          if (at !== ht) {
+            const match: Match = {
+              home: acceptedClubs[ht],
+              away: acceptedClubs[at],
+              id: matchIdGen(),
+              teams: [acceptedClubs[ht], acceptedClubs[at]],
+            };
+            const docRef = firestore
+              .collection("leagues")
+              .doc(leagueId)
+              .collection("matches2")
+              .doc();
+
+            batch.set(docRef, match);
+          }
+        }
+      }
+    }
+    batch.commit().catch((err) => {
+      console.log("something went wrong during batch commit:", err);
+    });
+  };
+
+  getTeams()
+    .then(() => createMatches())
+    .then(
+      () => {
+        return "Matches are scheduled and leagues can start!";
+      },
+      (err) => {
+        console.log("function failed", err);
+      }
+    );
+});
