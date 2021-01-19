302119251cfe4530246242952d52731d99ff2bc0
ziyafenn <ziya.fenn@gmail.com>
2021-01-19 18:06:46 +0100

    delete leagues, default init

---
+++ functions/src/deleteLeague.ts
@@ -0,0 +1,71 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { ClubInt } from "./utils/interface";
+const firebase_tools = require("firebase-tools");
+
+const logger = functions.logger;
+
+export const deleteLeague = functions
+  .runWith({
+    timeoutSeconds: 540,
+    memory: "2GB",
+  })
+  .https.onCall(async (data, context) => {
+    logger.info("data", data);
+
+    const firestore = admin.firestore();
+    const batch = firestore.batch();
+    const leagueId = data.leagueId;
+    const adminId = context.auth?.uid;
+    const isAdmin = context.auth && data.leagueAdminId === adminId;
+    const firTools = firebase_tools.firestore;
+    const users = firestore.collection("users");
+    const leagueClubs = await firestore
+      .collection("leagues")
+      .doc(leagueId)
+      .collection("clubs")
+      .get();
+    const path = `/leagues/${leagueId}`;
+
+    if (!isAdmin) {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "Must be league admin to initiate delete."
+      );
+    }
+
+    if (!leagueClubs.empty) {
+      leagueClubs.forEach((doc) => {
+        const club = doc.data() as ClubInt;
+        for (const memberId of Object.keys(club.roster)) {
+          const userRef = users.doc(memberId);
+          if (memberId !== data.leagueAdminId) {
+            batch.update(userRef, {
+              ["leagues." + leagueId]: admin.firestore.FieldValue.delete(),
+            });
+          }
+        }
+      });
+    }
+    const adminRef = users.doc(data.leagueAdminId);
+    batch.update(adminRef, {
+      ["leagues." + leagueId]: admin.firestore.FieldValue.delete(),
+    });
+    return batch
+      .commit()
+      .then(async () => {
+        await firTools.delete(path, {
+          project: process.env.GCLOUD_PROJECT,
+          recursive: true,
+          yes: true,
+          //     token: functions.config().fb.token,
+        });
+      })
+      .then(() => {
+        return "Success";
+      })
+      .catch((error) => {
+        console.log("something went wrong", error);
+        return "Something went wrong, please report";
+      });
+  });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,14 +2,11 @@ import * as admin from "firebase-admin";
 import * as schedule from "./scheduleMatches";
 import * as match from "./matchSubmission";
 import * as conflictedMatch from "./conflictResolution";
+import * as deleteUserLeague from "./deleteLeague";
 
-const serviceAccount = require("../keys/pro-clubs-zone-v2-firebase-adminsdk-jnm79-b0e14206de.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://pro-clubs-zone-v2.firebaseio.com",
-});
+admin.initializeApp();
 
 export const scheduleMatches = schedule.scheduleMatches;
 export const matchSubmission = match.matchSubmission;
 export const conflictResolution = conflictedMatch.conflictResolution;
+export const deleteLeague = deleteUserLeague.deleteLeague;
